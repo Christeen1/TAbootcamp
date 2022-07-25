bf41a06e7e7c431c5892339d0d225dfc1ea6c305
Kristine Ringe <109912680+Christeen1@users.noreply.github.com>
2022-07-25 17:16:53 +0300

    Activity 3

---
+++ TABootcampJavaProject1/src/com/bootcampexcercise/module3/activity/FindLargest.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module3.activity;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        int i=5, k=3;
+        if (i>k)
+            System.out.println("i Is the largest");
+    else
+        System.out.println("k Is the largest");
+    }}
