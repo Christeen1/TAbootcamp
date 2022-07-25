77b11514606bb823b05a613d77bfd8390ba25f3e
Kristine Ringe <109912680+Christeen1@users.noreply.github.com>
2022-07-25 16:29:41 +0300

    Activity2

---
+++ TABootcampJavaProject1/src/com/bootcampexcercise/module3/activity/OperatorActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.module3.activity;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int i = 5;
+        int w = --i;
+        
+            System.out.println("Result =" +(i+w)); // answer is 6
+
+
+    }
+}
