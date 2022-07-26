4b84a8fcff62ab1590b498fd160d8fb60c292e24
Kristine Ringe <109912680+Christeen1@users.noreply.github.com>
2022-07-26 10:38:55 +0300

    6,7

---
+++ TABootcampJavaProject1/src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.activity;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int i = 11; i <= 20; i++){
+            System.out.println("Multiplication" + i);
+
+    for (int j = 11; j <= 20; j++)
+
+                System.out.println (i + "X" + j +" = " + i*j);
+            }
+        }
+}
---
+++ TABootcampJavaProject1/src/com/bootcampexcercise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,15 @@
+package com.bootcampexcercise.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+
+        int r = 1;
+
+        while (r < 100) {
+            r = r+2;
+            System.out.println(r+"Numbers before 100");
+
+        }
+
+    }
+}
