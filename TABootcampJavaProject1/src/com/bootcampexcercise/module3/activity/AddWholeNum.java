2f04301ba359e7eb08e41d3ad175bf9ad96f0267
Kristine Ringe <109912680+Christeen1@users.noreply.github.com>
2022-07-26 09:30:04 +0300

    Activity 5

---
+++ TABootcampJavaProject1/src/com/bootcampexcercise/module3/activity/AddWholeNum.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module3.activity;
+
+public class AddWholeNum {
+    public static void main(String[] args) {
+        int sum=50;
+                for (int i=51; i<=100; i++)
+                    sum = sum + i;
+
+System.out.println("Sum is" + sum);
+}}
